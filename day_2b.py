9fdfabd4eafd67d42f12ee813b9316b56410ade6
samridhivirk1 <Samridhi.24003@stu.upes.ac.in>
2025-12-13 21:00:48 +0530

    Add files via upload

---
+++ day_2b.py
@@ -0,0 +1,11 @@
+#print multiple lines
+print ("First program- Python print function")
+print ("It is delcared like this:")
+print ("print 'what to print'")
+
+#print any datatype
+print (17*13)
+
+#print multiple objects in one line
+print ("The sum of 17 & 13", "is", 17+13)
+print("I love coading", 1+2, True)
